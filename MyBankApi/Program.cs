2980d4f535f6c9dac985ebfce0f1529750bd84fb
antonshinkevich <antons.hinkevich@gmail.com>
2023-02-12 11:44:27 +0300

    Update solution dependencies

--- MyBankApi/Program.cs
+++ MyBankApi/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<BankDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddMapster();
 
 var app = builder.Build();
 
---
+++ ServiceLayer/AtmService.cs
@@ -0,0 +1,24 @@
+﻿using DataEntityAndAccessLayer;
+using DataEntityAndAccessLayer.Entities.Atm;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServiceLayer;
+
+public class AtmService
+{
+    private readonly BankDbContext _context;
+
+    public AtmService(BankDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Atm>> GetAllAtms()
+    {
+        return await _context.Atms
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+
+}
